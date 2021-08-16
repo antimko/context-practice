a62133e65c2d5c5161778cc2933beff5c2f3dbb5
Andrejus Timko <timcostudios@gmail.com>
2021-08-16 09:06:12 +0300

    deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 
-cart = [{ id: 1, title: 'batai', price: 50 }];
+const cart = [{ id: 1, title: 'batai', price: 50 }];
 
 function App() {
   return <div className='App'>Hello</div>;
